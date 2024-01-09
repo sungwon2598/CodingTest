dc287e9d68d31475cc7b6a86795934a351e33caf
sungwon2598 <tjddnjs2598@gmail.com>
2024-01-09 20:22:08 +0900

    bj14501 : 퇴사 dp알고리즘

--- src/main/java/com/example/bj14501/Main.java
+++ src/main/java/com/example/bj14501/Main.java
@@ -3,29 +3,27 @@ package com.example.bj14501;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n + 1][2];
-        for (int i = 1; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-        int[] dp = new int[n + 1];
+
+        int arr[][] = new int[2][n + 1];
         for (int i = 1; i <= n; i++) {
-            int p = i;
-            while (p < n) {
-                dp[i] += arr[p][1];
-                p += arr[p][0];
-                System.out.println(p);
+            String input[] = br.readLine().split(" ");
+            arr[0][i] = Integer.parseInt(input[0]); // 기간
+            arr[1][i] = Integer.parseInt(input[1]); // 보수
+        }
+
+        int dp[] = new int[n + 2];
+        for (int i = n; i >= 1; i--) {
+            if (n + 1 - i >= arr[0][i]) {
+                dp[i] = Math.max(arr[1][i] + dp[i + arr[0][i]], dp[i + 1]);
+            } else {
+                dp[i] = dp[i + 1];
             }
         }
-        Arrays.sort(dp);
-        System.out.println(dp[n]);
+        System.out.println(dp[1]);
     }
 }
